e137d368f6c35fc5b7e367e18e16dd30420257e8
GiToon10100011 <Boon10034@gmail.com>
2025-04-12 21:46:20 +0900

    get soptify hot tracks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.scdn.co"], // Allow images from Spotify's CDN
+  },
 };
 
 export default nextConfig;
